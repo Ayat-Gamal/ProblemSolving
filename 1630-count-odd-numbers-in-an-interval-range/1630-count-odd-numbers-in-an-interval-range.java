dbf5d7d51676d1e2cbf9a653880b72e1b6507a8a
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-03-11 04:35:55 +0200

    Time: 0 ms (100%), Space: 40.5 MB (53.48%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.java
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.java
@@ -1,27 +1,26 @@
 class Solution {
 
-
-    /** low, , , ,high => (high -low +1) if even =>n/2 
-    if odd (low < hith) = n/2 +1
-        3 , 5      ,7 =>)(3)   7- 3 = 4 -1 =3
-        8,  9,  10 =>(3)    3/2 => 1
-
-        1, 3,5,7,  ,8 =>(8) n/2 = 8/2 =4
-        5, 7,9 ,10 => (6)   6/3= 3
-        1,3,5,   6 =>(6)  6/2 = 3
-             */
+    /**
+     * low, , , ,high => (high -low +1) if even =>n/2
+     * if odd (low < hith) = n/2 +1
+     * 3 , 5 ,7 =>)(3) 7- 3 = 4 -1 =3
+     * 8, 9, 10 =>(3) 3/2 => 1
+     * 
+     * 1, 3,5,7, ,8 =>(8) n/2 = 8/2 =4
+     * 5, 7,9 ,10 => (6) 6/3= 3
+     * 1,3,5, 6 =>(6) 6/2 = 3
+     */
     public int countOdds(int low, int high) {
-        int N = high - low +1;
 
-        if(low %2 ==0  && high % 2 == 0){
-            return (high - low)/2;
-        }else if(low %2 !=0  && high % 2 != 0)
-        {
-            if(low == high) return 1;
-            return (high - low -1)/2+2;
-        }else {
-            return (high - low -1)/2+1;
+        if (low % 2 == 0 && high % 2 == 0) {
+            return (high - low) / 2;
+        } else if (low % 2 != 0 && high % 2 != 0) {
+            if (low == high)
+                return 1;
+            return (((high - low - 1) / 2 )+ 2);
+        } else {
+            return (((high - low - 1) / 2) + 1);
 
-        } 
+        }
     }
 }
