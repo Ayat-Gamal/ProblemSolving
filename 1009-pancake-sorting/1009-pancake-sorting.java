e9783c05199ce4b78300ab8fb8d4a68d299837d6
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-03-13 17:16:03 +0200

    Time: 1 ms (98.35%), Space: 42.7 MB (56.6%) - LeetHub

--- 1009-pancake-sorting/1009-pancake-sorting.java
+++ 1009-pancake-sorting/1009-pancake-sorting.java
@@ -1,19 +1,21 @@
 class Solution {
+
     public List<Integer> pancakeSort(int[] arr) {
         List<Integer> result = new ArrayList<>();
         int n = arr.length;
         for (int i = n; i > 0; i--) {
 
             int maxElemnt = findMax(arr, i);
+            if (maxElemnt == i - 1) {
+                continue;
+            }
 
             if (maxElemnt != 0) {
                 flip(arr, maxElemnt + 1);
                 result.add(maxElemnt + 1);
             }
-
             flip(arr, i);
             result.add(i);
-
         }
         return result;
     }
@@ -30,7 +32,7 @@ class Solution {
     void flip(int[] arr, int k) {
         int left = 0;
         int right = k - 1;
-        while (left< right) {
+        while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
