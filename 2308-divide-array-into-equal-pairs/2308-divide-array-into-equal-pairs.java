7ec73030afff299f5d9b5b6bec37d6d2d06248e4
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-03-17 21:33:31 +0200

    Time: 6 ms (45.22%), Space: 45.3 MB (5.18%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,20 +1,39 @@
 class Solution {
+    /**
+     * we can  use HashSet() !! wow O(n),, also HashMap sapce O(n)
+     * add and remove the element, at end if empty => true lese fasle 
+     */
 
     public boolean divideArray(int[] nums) {
-        int n = nums.length;
-        if (n % 2 != 0)
+
+        if (nums.length % 2 != 0)
             return false;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for (int count : map.values()) {
-            if (count % 2 != 0) {
-                return false;
+        HashSet set = new HashSet<Integer>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
             }
         }
-        return true;
-
+        return set.isEmpty();
+        /*
+         * int n = nums.length;
+         * if (n % 2 != 0)
+         * return false;
+         * 
+         * HashMap<Integer, Integer> map = new HashMap<>();
+         * for (int i = 0; i < n; i++) {
+         * map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+         * }
+         * for (int count : map.values()) {
+         * if (count % 2 != 0) {
+         * return false;
+         * }
+         * }
+         * return true;
+         */
     }
 }
