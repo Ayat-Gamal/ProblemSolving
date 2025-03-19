cf789ed2031341222c3b58ded4fbc18dd988b23e
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-03-19 22:08:55 +0200

    Time: 1 ms (82.67%), Space: 43.6 MB (78.39%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        int pos = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && nums[i] == nums[i + 1] ) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
-        }
-        int pos = 0;
-        for (int i = 0; i < nums.length; i++) {
+
             if (nums[i] != 0) {
                 int temp = nums[pos];
                 nums[pos] = nums[i];
@@ -15,6 +15,7 @@ class Solution {
                 pos++;
             }
         }
+
         return nums;
 
     }
