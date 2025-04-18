e50c3e3fdad14ca647a6af6cdcd47b74455e3b9d
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-04-18 15:07:26 +0200

    Time: 0 ms (100%), Space: 57.7 MB (22.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,10 +3,20 @@ class Solution {
     /**
     use reminder to get postion of moved eleemnts === k % n 
      as n umber of elemnts ina array
-
-     solution 1 = use extra space
+    
+     solution 1 = use extra space  space O(N)
+     solution 2 = in palce array,, we will use reverse
+    
      */
     public void rotate(int[] nums, int k) {
+        //  solution 2
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+        /*    
+            //  solution 1
             int n = nums.length;
               k = k % n;
                 int [] rotated = new int [n]; 
@@ -16,7 +26,19 @@ class Solution {
                for(int i = 0; i< n ; i++){
                     nums[i ] = rotated[i];
               }
+        
+            */
+    }
 
-            
+    private void reverse(int[] nums, int left, int right) {
+
+        while (left < right) {
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
