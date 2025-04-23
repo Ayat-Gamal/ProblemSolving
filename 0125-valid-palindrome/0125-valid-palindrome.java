dd1a345dc25ac36ae98e1c6ff4308e4010a9e6c6
Ayat Gamal <90482904+AyatGaa@users.noreply.github.com>
2025-04-23 10:27:25 +0200

    Time: 2 ms (99.04%), Space: 43 MB (76.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,31 @@
 class Solution {
-  
+
     public boolean isPalindrome(String s) {
-    
+        //instead of convert the whole string 
+        int left =0;
+        int right = s.length()-1;
+        
+         while(left< right){
+            
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left)))
+            {
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            //make sure that the left and right are letter or digit and then convert to lower case and check!!!
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)) ){
+                return false;
+            }
+            left ++;
+            right--;
+        }
+
+return true;
+
+
+       /*
         s = s.toLowerCase();
         s = s.replaceAll("[^a-z0-9]","");
         int left =0;
@@ -16,6 +40,6 @@ class Solution {
             right--;
         }
 
-        return true;
+        return true;*/
     }
 }
