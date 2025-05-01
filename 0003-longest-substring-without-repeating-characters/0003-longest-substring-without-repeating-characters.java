92ac9a554fee9dd3dacdf2c80c230482ed1d5dd0
Ayat Gamal <90482904+Ayat-Gamal@users.noreply.github.com>
2025-05-01 16:25:14 +0300

    Time: 5 ms (87.84%), Space: 44.7 MB (68.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,9 +1,29 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        //HashMap
+        Map<Character, Integer> mp = new HashMap<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (mp.containsKey(ch)) {
+
+                left = Math.max(left, mp.get(ch) + 1);
+            }
+            mp.put(ch, i);
+            maxLength = Math.max(maxLength, i - left + 1);
+        }
+
+        return maxLength;
+
+        /*
+        //Set
         Set<Character> hs = new HashSet<>();
         int left = 0;
         int maxLength = 0;
- 
+        
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             while (hs.contains(ch)) {
@@ -11,11 +31,11 @@ class Solution {
                 left++;
             }
             hs.add(ch);
-
+        
             maxLength = Math.max(maxLength, i - left + 1); //the sindow size!
-
+        
         }
-
-        return maxLength;
+        
+        return maxLength; */
     }
 }
