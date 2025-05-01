56662548370930b85a7f0fa4333fa0c5f337b35c
Ayat Gamal <90482904+Ayat-Gamal@users.noreply.github.com>
2025-05-01 16:15:08 +0300

    Time: 6 ms (70.48%), Space: 44.8 MB (42.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,22 +2,20 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> hs = new HashSet<>();
         int left = 0;
-        int maxLength = 1;
-if (s.isEmpty()) {
-    return 0;
-}
-        for(int i =0 ;i<s.length() ;i++){
+        int maxLength = 0;
+ 
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            while(hs.contains(ch)){
+            while (hs.contains(ch)) {
                 hs.remove(s.charAt(left));
-               left++;
+                left++;
             }
             hs.add(ch);
 
-            maxLength  =Math.max(maxLength , i-left +1); //the sindow size!
+            maxLength = Math.max(maxLength, i - left + 1); //the sindow size!
 
         }
 
-return maxLength; 
+        return maxLength;
     }
 }
